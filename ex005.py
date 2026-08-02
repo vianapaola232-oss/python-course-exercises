24401e4c02ea000c83bfb7c2fea4afc08d521b8a
vianapaola232-oss <vianapaola232@gmail.com>
2026-08-01 21:04:21 -0300

    Predecessor and successor

---
+++ ex005.py
@@ -0,0 +1,6 @@
+num = int(input('Type a number: '))
+
+successor = num + 1
+predecessor = num - 1
+
+print(f'Analyzing the value {num}, its predecessor is {predecessor} and its successor is {successor}.')
