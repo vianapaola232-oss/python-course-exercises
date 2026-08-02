fe0fa5858956d8a875370a9b0ee766b1e5e0f500
vianapaola232-oss <vianapaola232@gmail.com>
2026-08-01 21:15:26 -0300

    Double, triple and square root.

---
+++ ex006.py
@@ -0,0 +1,5 @@
+num = int(input('Type a number: '))
+double = num * 2
+triple = num * 3
+square_root = num ** 0.5
+print(f'Analyzing the value {num}, its double is {double}, its triple is {triple} and its square root is {square_root:.2f}.')
