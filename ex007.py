21a0e50f163c55766ec7d9c9b25e7fb3a15bcf31
vianapaola232-oss <vianapaola232@gmail.com>
2026-08-01 21:32:27 -0300

    Arithmetic mean

---
+++ ex007.py
@@ -0,0 +1,6 @@
+grade1 = float(input("Enter the student's first grade: "))
+grade2 = float(input("Enter the student's second grade: "))
+
+arithmetic_mean = (grade1 + grade2) / 2
+
+print(f"The arithmetic mean of {grade1:.2f} and {grade2:.2f} is {arithmetic_mean:.2f}.")
