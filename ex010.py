a5f9405a81de3f78fcd69f1bead2615303edd02d
vianapaola232-oss <vianapaola232@gmail.com>
2026-08-01 22:23:58 -0300

    Currency converter

---
+++ ex010.py
@@ -0,0 +1,10 @@
+dollars = float(input("How much money do you have in your wallet? US$"))
+
+euros = dollars * 0.87
+
+print("-=" * 30)
+print("CONVERTING US DOLLARS TO EUROS")
+print("-=" * 30)
+print(f"US${dollars:.2f} equals €{euros:.2f}.")
+
+
