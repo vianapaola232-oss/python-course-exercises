7b429946de1e46805856a0ca1a8e5530c4aa936e
vianapaola232-oss <vianapaola232@gmail.com>
2026-08-03 22:31:09 -0300

    Discount

--- ex012.py
+++ ex012.py
@@ -1,4 +1,4 @@
-product = float(input("What is the price of the product? US$"))
-discount = product * (5 / 100)
-new_price = product - discount
+product_price = float(input("What is the price of the product? US$"))
+discount = product_price * (5 / 100)
+new_price = product_price - discount
 print(f"The price of the product after the discount is US${new_price:.2f}.")
