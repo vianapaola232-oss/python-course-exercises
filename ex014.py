3ccb767024750ca71c7846adb47ebb9c17b9c2f9
vianapaola232-oss <vianapaola232@gmail.com>
2026-08-20 19:22:10 -0300

    Temperature converter

---
+++ ex014.py
@@ -0,0 +1,3 @@
+celsius_temp = float(input("How many degrees Celsius is the temperature? : "))
+fahrenheit_temp = (celsius_temp * 9 / 5) + 32
+print(f"The temperature {celsius_temp}ºC corresponds to {fahrenheit_temp}ºF.")
