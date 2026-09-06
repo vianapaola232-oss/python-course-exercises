ab06412cd007396425de822f949d69d44b02913a
vianapaola232-oss <vianapaola232@gmail.com>
2026-09-05 22:02:47 -0300

    A person's first and last name

---
+++ ex027.py
@@ -0,0 +1,4 @@
+full_name = str(input("Please, enter your full name: ")).strip()
+first_last = full_name.split()
+print(f"Your first name is {first_last[0]}.")
+print(f"Your last name is {first_last[-1]}.")
